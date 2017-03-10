b7b691dbef086b017422c04d7c9edbf9c23b312f
Onur Yıldırım <onur@cutepilot.com>
2017-03-11 02:25:39 +0300

    add Possession enumeration

---
+++ src/enums/Possession.ts
@@ -0,0 +1,24 @@
+/**
+ *  Enumerates the possible possessions of a resource, for an action.
+ *  A possession defines whether the action is (or not) to be performed on "own"
+ *  resource(s) of the current subject or "any" resources - including "own".
+ *  @enum {String}
+ *  @readonly
+ *  @memberof! AccessControl
+ */
+const Possession = {
+    /**
+     *  Indicates that the action is (or not) to be performed on <b>own</b>
+     *  resource(s) of the current subject.
+     *  @type {String}
+     */
+    OWN: 'own',
+    /**
+     *  Indicates that the action is (or not) to be performed on <b>any</b>
+     *  resource(s); including <i>own</i> resource(s) of the current subject.
+     *  @type {String}
+     */
+    ANY: 'any'
+};
+
+export default Possession;
