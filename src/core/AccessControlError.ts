0c21c3f083292db587cfe30522867dea54e9a542
Onur Yıldırım <onur@cutepilot.com>
2017-03-11 02:24:29 +0300

    error class specific to AC

---
+++ src/core/AccessControlError.ts
@@ -0,0 +1,17 @@
+/**
+ *  Error class specific to `AccessControl`.
+ *  @readonly
+ *  @name AccessControl.Error
+ *  @class
+ *  @static
+ */
+class AccessControlError extends Error {
+    public name:string = 'AccessControlError';
+    constructor(public message:string = '') {
+        super(message);
+        // http://stackoverflow.com/a/41429145/112731
+        Object.setPrototypeOf(this, AccessControlError.prototype);
+    }
+}
+
+export default AccessControlError;
