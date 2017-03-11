70e233ee37d5c5b8600d1e726cb7282fa570bd83
Onur Yıldırım <onur@cutepilot.com>
2017-03-11 03:06:42 +0300

    enums index

---
+++ src/enums/index.ts
@@ -0,0 +1,12 @@
+import Action from './Action';
+import Possession from './Possession';
+
+const actions:string[] = Object.keys(Action).map((k:string) => Action[k]);
+const possessions:string[] = Object.keys(Possession).map((k:string) => Possession[k]);
+
+export {
+    Action,
+    actions,
+    Possession,
+    possessions
+};
