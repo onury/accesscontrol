0af9c16442d40db69c27dc25a78e360ffc5e6bad
Onur Yıldırım <onur@cutepilot.com>
2017-05-24 05:01:37 +0300

    better imports

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./lib/AccessControl').default;
+var AC = require('./lib').AccessControl;
+module.exports = AC;
+// adding circular ref to allow easy importing in both ES5/6 and TS projects
+module.exports.AccessControl = AC;
