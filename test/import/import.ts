674bc5d66906159bbeef61aa422303840262448c
Onur Yıldırım <onur@cutepilot.com>
2017-11-21 03:41:15 +0300

    minor

--- test/import/import.ts
+++ test/import/import.ts
@@ -1,6 +1,6 @@
 import { AccessControl } from '../../';
 
 console.log(AccessControl);
-let ac = new AccessControl();
+const ac = new AccessControl();
 ac.grant('user').createAny('resource');
 console.log(ac.getGrants());
