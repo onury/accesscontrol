d1e89009cdd2bfa78e1b9eef24a109a01543014a
Onur Yıldırım <onur@cutepilot.com>
2017-11-26 06:18:21 +0300

    update helper

--- test/helper.ts
+++ test/helper.ts
@@ -6,6 +6,7 @@
  */
 
 import { AccessControl } from '../src';
+import { AccessControlError } from '../src/core';
 
 const helper = {
     expectACError(fn: any, errMsg?: string) {
@@ -14,6 +15,7 @@ const helper = {
             fn();
         } catch (err) {
             expect(err instanceof AccessControl.Error).toEqual(true);
+            expect(err instanceof AccessControlError).toEqual(true);
             expect(AccessControl.isAccessControlError(err)).toEqual(true);
             expect(AccessControl.isACError(err)).toEqual(true); // alias test
             if (errMsg) expect(err.message).toContain(errMsg);
