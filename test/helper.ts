5269eefe94b17b8f22f137090e08048fc90adaf0
Onur Yıldırım <onury@users.noreply.github.com>
2026-06-20 00:02:04 +0300

    Update helper.ts

--- test/helper.ts
+++ test/helper.ts
@@ -1,27 +1,16 @@
-'use strict';
+import { AccessControlError } from '../src/core/index.js';
+import { AccessControl } from '../src/index.js';
 
-/**
- *  Test Helper
- *  @author   Onur Yıldırım <onur@cutepilot.com>
- */
-
-import { AccessControl } from '../src';
-import { AccessControlError } from '../src/core';
-
-const helper = {
-    expectACError(fn: any, errMsg?: string) {
-        expect(fn).toThrow();
-        try {
-            fn();
-        } catch (err) {
-            expect(err instanceof AccessControl.Error).toEqual(true);
-            expect(err instanceof AccessControlError).toEqual(true);
-            expect(AccessControl.isAccessControlError(err)).toEqual(true);
-            expect(AccessControl.isACError(err)).toEqual(true); // alias test
-            if (errMsg) expect(err.message).toContain(errMsg);
-        }
+export const helper = {
+  expectACError(fn: any, errMsg?: string) {
+    expect(fn).toThrow();
+    try {
+      fn();
+    } catch (err) {
+      expect(err instanceof AccessControlError).toEqual(true);
+      expect(AccessControl.isACError(err)).toEqual(true);
+      expect(AccessControl.isACError(err)).toEqual(true); // alias test
+      if (errMsg) expect((err as Error).message).toContain(errMsg);
     }
-
+  }
 };
-
-export { helper };
