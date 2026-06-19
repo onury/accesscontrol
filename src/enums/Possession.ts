7b0600705a12cf0270c606f542bf75aa02f1ee49
Onur Yıldırım <onury@users.noreply.github.com>
2026-06-20 00:02:51 +0300

    Update Possession.ts

--- src/enums/Possession.ts
+++ src/enums/Possession.ts
@@ -1,24 +1,17 @@
 /**
- *  Enumerates the possible possessions of a resource, for an action.
- *  A possession defines whether the action is (or not) to be performed on "own"
- *  resource(s) of the current subject or "any" resources - including "own".
- *  @enum {String}
- *  @readonly
- *  @memberof! AccessControl
+ * Enumerates the possible possessions of a resource, for an action.
+ * A possession defines whether the action is (or not) to be performed on "own"
+ * resource(s) of the current subject or "any" resources - including "own".
  */
-const Possession = {
-    /**
-     *  Indicates that the action is (or not) to be performed on <b>own</b>
-     *  resource(s) of the current subject.
-     *  @type {String}
-     */
-    OWN: 'own',
-    /**
-     *  Indicates that the action is (or not) to be performed on <b>any</b>
-     *  resource(s); including <i>own</i> resource(s) of the current subject.
-     *  @type {String}
-     */
-    ANY: 'any'
-};
-
-export { Possession };
+export enum Possession {
+  /**
+   *  Indicates that the action is (or not) to be performed on <b>own</b>
+   *  resource(s) of the current subject.
+   */
+  OWN = 'own',
+  /**
+   *  Indicates that the action is (or not) to be performed on <b>any</b>
+   *  resource(s); including <i>own</i> resource(s) of the current subject.
+   */
+  ANY = 'any'
+}
